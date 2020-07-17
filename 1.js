b2d4839e07e267662bd378b8c2e4e974f5410dfc
李朋 <15646507519@sian.cn>
2020-07-17 18:41:35 +0800

    f

--- 1.js
+++ 1.js
@@ -1,7 +1,37 @@
-function vehicle(vehicleType) {
-    this.vehicleType = vehicleType || "car";
-    this.model = "default";
-    this.license = "00000-000";
-}
-var truck = new vehicle( "truck" );
-console.log(truck);
+let express = require('express');
+let app = express();
+
+let bodyParser = require('body-parser');
+/*
+   body-parser介绍：https://www.jianshu.com/p/cd3de110b4b6
+   当请求体解析之后，解析值会被放到req.body属性中，当内容为空时候，为一个空对象{}
+   ---bodyParser.json()--解析JSON格式
+   ---bodyParser.raw()--解析二进制格式
+   ---bodyParser.text()--解析文本格式
+   ---bodyParser.urlencoded()--解析文本格式
+*/
+// app.use(); 会拦截每一次的请求
+
+// 解析 application/json
+let jsonParser = app.use(bodyParser.json());
+// 解析 application/x-www-form-urlencoded
+let urlencodedParser = app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// 静态文件
+app.use('/',express.static(__dirname + '/static'));
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+app.post('/login', urlencodedParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    console.log(req)
+    res.send('welcome, ' + req.body.username);
+})
+
+
+app.listen(5554)
