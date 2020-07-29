8bdb501a9c2f9a387c593a2fd08ab38990651f5c
lipeng <15646507519@sina.cn>
2020-07-29 15:50:54 +0800

    f

---
+++ router/router.js
@@ -0,0 +1,17 @@
+let express = require('express');
+var Router = express.Router();
+
+Router.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// 获取路由参数
+Router.param('ids', function (req, res, next, ids) {
+    req.ids = ids;
+    next();
+})
+Router.get('/:ids', (req, res) => {
+    res.send('Hello World!' + req.ids)
+})
+
+module.exports = Router;
